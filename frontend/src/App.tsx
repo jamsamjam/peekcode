f17da9f474342f5e0a546f27ac7ea481d44eacfa
jamsamjam <andsunlit@gmail.com>
2025-11-29 22:25:04 +0100

    use easy-peasy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import { useState, type FormEvent } from 'react'
 import './App.css'
+import { useStoreActions, useStoreState } from './store';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [hasAccount, setHasAccount] = useState(false);
+  const [problems, setProblems] = useState([]);
+  const jwt = useStoreState((state) => state.jwt);
+  const setToken = useStoreActions((actions) => actions.setToken);
 
   const handleRegister = async (event: FormEvent<HTMLFormElement>) => {
 
@@ -35,7 +38,6 @@ function App() {
   }
 
   const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
-
     event.preventDefault();
 
     const formElement = event.currentTarget;
@@ -55,13 +57,35 @@ function App() {
       return;
     }
 
-    setIsLoggedIn(true);
+    const data = await response.json();
+    setToken(data.token);
+  }
+
+  const showProblems = async () => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_BASE_URL}/api/v1/problems/getProblems`, {
+        method: "GET",
+        headers: {
+          'Authorization': `Bearer ${jwt}`,
+        }
+      })
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch problems');
+      }
+
+      const data = await response.json();
+      setProblems(data);
+    } catch (error) {
+      console.error('Error fetching problems:', error);
+      alert('Failed to load problems');
+    } 
   }
 
   return (
     <>
       <h1>PeekCode</h1>
-      {!isLoggedIn && (
+      {!jwt && (
         !hasAccount ? (
           <form onSubmit={handleRegister}>
             <label htmlFor="email">Email: </label>
@@ -84,9 +108,18 @@ function App() {
         )
       )}
 
-      {isLoggedIn && (
-        <>Welcome hello hello</>
+      {jwt && (
+        <>
+          <h2>My Problems</h2>
+          {problems.length === 0 ? (
+            <p>No problems found</p>
+          ) : (
+            <></>
+          )}
+        </>
       )}
+
+
     </>
   )
 }
---
+++ frontend/src/store.ts
@@ -0,0 +1,23 @@
+import { createStore, action, createTypedHooks } from 'easy-peasy';
+import type { Action } from 'easy-peasy';
+import { persist } from 'easy-peasy';
+
+interface PeekCodeStore {
+    jwt: string|null,
+    setToken: Action<PeekCodeStore, string>
+}
+
+export const store = createStore<PeekCodeStore>(
+    persist({
+      jwt: null,
+      setToken: action((state, payload) => {
+        state.jwt = payload;
+      }),
+    })
+  );
+
+const typedHooks = createTypedHooks<PeekCodeStore>();
+
+export const useStoreActions = typedHooks.useStoreActions;
+export const useStoreDispatch = typedHooks.useStoreDispatch;
+export const useStoreState = typedHooks.useStoreState;
