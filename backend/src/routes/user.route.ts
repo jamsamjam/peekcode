5cc98b258b15856e2ad63683ff37e363db37a467
jamsamjam <andsunlit@gmail.com>
2025-12-03 02:01:28 +0100

    fix: user/:id -> get method

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -3,6 +3,6 @@ import { getUser } from '../controllers/user.controller.js';
 
 const router = Router();
 
-router.route('/user/:id').post(getUser);
+router.route('/user/:id').get(getUser);
 
 export default router;
